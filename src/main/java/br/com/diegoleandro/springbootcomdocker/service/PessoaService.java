9855a007fbb090426cbd7140c23c8c39d2e6f2f0
Diego leandro <diegolgaspar000@gmail.com>
2022-03-22 20:19:30 -0300

    refatorando classe service

--- src/main/java/br/com/diegoleandro/springbootcomdocker/service/PessoaService.java
+++ src/main/java/br/com/diegoleandro/springbootcomdocker/service/PessoaService.java
@@ -2,7 +2,6 @@ package br.com.diegoleandro.springbootcomdocker.service;
 
 import br.com.diegoleandro.springbootcomdocker.model.Pessoa;
 import br.com.diegoleandro.springbootcomdocker.repository.PessoaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,7 +9,6 @@ import java.util.List;
 @Service
 public class PessoaService {
 
-    @Autowired
     private PessoaRepository pessoaRepository;
 
     public PessoaService(PessoaRepository pessoaRepository) {
