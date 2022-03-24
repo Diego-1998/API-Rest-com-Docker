0cfff3015ecb508cfbafdb9478247ae5d3c662e3
Diego leandro <diegolgaspar000@gmail.com>
2022-03-24 20:58:44 -0300

    adicionando métodos update e post no projeto

--- src/main/java/br/com/diegoleandro/springbootcomdocker/controller/PessoaController.java
+++ src/main/java/br/com/diegoleandro/springbootcomdocker/controller/PessoaController.java
@@ -1,10 +1,11 @@
 package br.com.diegoleandro.springbootcomdocker.controller;
 
+import br.com.diegoleandro.springbootcomdocker.controller.dto.PessoaRequest;
 import br.com.diegoleandro.springbootcomdocker.controller.dto.PessoaResponse;
+import br.com.diegoleandro.springbootcomdocker.model.Pessoa;
+import br.com.diegoleandro.springbootcomdocker.repository.PessoaRepository;
 import br.com.diegoleandro.springbootcomdocker.service.PessoaService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,9 +14,12 @@ import java.util.stream.Collectors;
 @RequestMapping("/pessoa")
 public class PessoaController {
 
+
+    private final PessoaRepository pessoaRepository;
     private final PessoaService pessoaService;
 
-    public PessoaController(PessoaService pessoaService) {
+    public PessoaController(PessoaRepository pessoaRepository, PessoaService pessoaService) {
+        this.pessoaRepository = pessoaRepository;
         this.pessoaService = pessoaService;
     }
 
@@ -27,4 +31,34 @@ public class PessoaController {
                 .map(PessoaResponse::converter)
                 .collect(Collectors.toList());
     }
+
+    @GetMapping("/{pessoaId}")
+    public PessoaResponse findById(@PathVariable Long pessoaId){
+        var pessoa = pessoaRepository.getById(pessoaId);
+        return PessoaResponse.converter(pessoa);
+    }
+
+    @PostMapping("/")
+    public void savePerson(@RequestBody PessoaRequest pessoaRequest){
+        var pessoa = new Pessoa();
+        pessoa.setNome(pessoaRequest.getNome());
+        pessoa.setSobrenome(pessoaRequest.getSobrenome());
+        pessoaRepository.save(pessoa);
+    }
+
+    @PutMapping("/{pessoaId}")
+    public void updatePerson(@PathVariable Long pessoaId,@RequestBody PessoaRequest pessoaRequest) throws Exception {
+        var pessoa = pessoaRepository.findById(pessoaId);
+
+        if(pessoa.isPresent()){
+            var pessoaSave = pessoa.get();
+            pessoaSave.setNome(pessoaRequest.getNome());
+            pessoaSave.setSobrenome(pessoaRequest.getSobrenome());
+            pessoaRepository.save(pessoaSave);
+        }
+        else{
+            throw new Exception("Pessoa não encontrada");
+        }
+    }
+
 }
