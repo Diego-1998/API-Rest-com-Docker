a8475f4ab85a6e44bd5c676c970d17a9b5d97b42
Diego leandro <diegolgaspar000@gmail.com>
2022-03-23 23:51:26 -0300

    avançando com ambiente docker

--- src/main/java/br/com/diegoleandro/springbootcomdocker/controller/PessoaController.java
+++ src/main/java/br/com/diegoleandro/springbootcomdocker/controller/PessoaController.java
@@ -1,9 +1,7 @@
 package br.com.diegoleandro.springbootcomdocker.controller;
 
 import br.com.diegoleandro.springbootcomdocker.controller.dto.PessoaResponse;
-import br.com.diegoleandro.springbootcomdocker.model.Pessoa;
 import br.com.diegoleandro.springbootcomdocker.service.PessoaService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
